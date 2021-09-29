09e4a735ca0aecc07b1f30b95a031ac0b7222789
Betuntun <36393621+Betuntun@users.noreply.github.com>
2021-09-29 02:23:07 -0600

    termine la appp de heroes

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +14,19 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class HomeComponent implements OnInit {
+  //auth!:Auth;
+  get auth(){
+    return this.authService.auth;
+  }
+  constructor(private router: Router, private authService:AuthService) { }
 
-  constructor() { }
+
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.router.navigate(['./auth']);
   }
 
 }
